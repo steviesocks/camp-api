47aad7157a7762cb8926c2bd7090c5f83b6f59d9
stephen <stephen.stocking@gmail.com>
2023-06-11 12:50:34 -0400

    add support for ELF, ELH, and GAB

--- api/dist/index.js
+++ api/dist/index.js
@@ -72,25 +72,32 @@ app.all("/", function (req, res) {
     res.send("Yooo!");
 });
 app.get("/cron", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var response, filteredDates, availability_1, requests, hasAvail, message, error_1;
+    var response, STO, filteredDates, availability_1, requests, hasAvail, message, error_1, ELF, filteredDates, availability_2, requests, hasAvail, message, error_2, ELH, filteredDates, availability_3, requests, hasAvail, message, error_3, GAB, filteredDates, availability_4, requests, hasAvail, message, error_4, error_5;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
-                _a.trys.push([0, 4, , 5]);
-                return [4 /*yield*/, axios_1.default.get("https://www.recreation.gov/api/permititinerary/4675321/division/4675321007/availability/month?month=8&year=2023")];
+                console.log("CRON!");
+                response = {};
+                _a.label = 1;
             case 1:
-                response = _a.sent();
-                filteredDates = Object.entries(response.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter(function (entry) {
+                _a.trys.push([1, 23, , 24]);
+                _a.label = 2;
+            case 2:
+                _a.trys.push([2, 6, , 7]);
+                return [4 /*yield*/, axios_1.default.get("https://www.recreation.gov/api/permititinerary/4675321/division/4675321007/availability/month?month=8&year=2023")];
+            case 3:
+                STO = _a.sent();
+                filteredDates = Object.entries(STO.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter(function (entry) {
                     return ["2023-08-11", "2023-08-12"].includes(entry[0]);
                 });
-                if (!filteredDates.length) return [3 /*break*/, 3];
+                if (!filteredDates.length) return [3 /*break*/, 5];
                 availability_1 = {};
                 filteredDates.forEach(function (date) {
                     availability_1[date[0]] = { total: date[1].total, remaining: date[1].remaining };
                 });
-                requests = (0, lite_1.collection)(db, "requests");
+                requests = (0, lite_1.collection)(db, "STO");
                 return [4 /*yield*/, (0, lite_1.addDoc)(requests, { dateTime: Date.now(), availability: availability_1 })];
-            case 2:
+            case 4:
                 _a.sent();
                 hasAvail = Object.entries(availability_1).filter(function (date) { return date[1].remaining > 0; });
                 if (hasAvail.length) {
@@ -101,28 +108,161 @@ app.get("/cron", function (req, res) { return __awaiter(void 0, void 0, void 0, 
                         .create({ body: message, from: "+18668414666", to: "+17163615473" })
                         .then(function (msg) { return console.log(msg.sid); });
                 }
-                _a.label = 3;
-            case 3: return [2 /*return*/, res.status(200).send({ dateTime: Date.now(), filteredDates: filteredDates })];
-            case 4:
+                _a.label = 5;
+            case 5:
+                response.STO = { dateTime: Date.now(), filteredDates: filteredDates };
+                return [3 /*break*/, 7];
+            case 6:
                 error_1 = _a.sent();
-                console.error(error_1);
-                return [2 /*return*/, res.status(500).send(error_1)];
-            case 5: return [2 /*return*/];
+                console.log("STO error", error_1);
+                return [3 /*break*/, 7];
+            case 7:
+                _a.trys.push([7, 11, , 12]);
+                return [4 /*yield*/, axios_1.default.get("https://www.recreation.gov/api/permititinerary/4675321/division/4675321020/availability/month?month=8&year=2023")];
+            case 8:
+                ELF = _a.sent();
+                filteredDates = Object.entries(ELF.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter(function (entry) {
+                    return ["2023-08-09", "2023-08-10"].includes(entry[0]);
+                });
+                if (!filteredDates.length) return [3 /*break*/, 10];
+                availability_2 = {};
+                filteredDates.forEach(function (date) {
+                    availability_2[date[0]] = { total: date[1].total, remaining: date[1].remaining };
+                });
+                requests = (0, lite_1.collection)(db, "ELF");
+                return [4 /*yield*/, (0, lite_1.addDoc)(requests, { dateTime: Date.now(), availability: availability_2 })];
+            case 9:
+                _a.sent();
+                hasAvail = Object.entries(availability_2).filter(function (date) { return date[1].remaining > 0; });
+                if (hasAvail.length) {
+                    message = "Hey! Elizabeth Lake Foot has availability: ".concat(hasAvail
+                        .map(function (date) { return date[0] + ": " + date[1].remaining + " remaining sites"; })
+                        .join(", "));
+                    twilioClient.messages
+                        .create({ body: message, from: "+18668414666", to: "+17163615473" })
+                        .then(function (msg) { return console.log(msg.sid); });
+                }
+                _a.label = 10;
+            case 10:
+                response.ELF = { dateTime: Date.now(), filteredDates: filteredDates };
+                return [3 /*break*/, 12];
+            case 11:
+                error_2 = _a.sent();
+                console.log("ELF error", error_2);
+                return [3 /*break*/, 12];
+            case 12:
+                _a.trys.push([12, 16, , 17]);
+                return [4 /*yield*/, axios_1.default.get("https://www.recreation.gov/api/permititinerary/4675321/division/4675321021/availability/month?month=8&year=2023")];
+            case 13:
+                ELH = _a.sent();
+                filteredDates = Object.entries(ELH.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter(function (entry) {
+                    return ["2023-08-09", "2023-08-10"].includes(entry[0]);
+                });
+                if (!filteredDates.length) return [3 /*break*/, 15];
+                availability_3 = {};
+                filteredDates.forEach(function (date) {
+                    availability_3[date[0]] = { total: date[1].total, remaining: date[1].remaining };
+                });
+                requests = (0, lite_1.collection)(db, "ELH");
+                return [4 /*yield*/, (0, lite_1.addDoc)(requests, { dateTime: Date.now(), availability: availability_3 })];
+            case 14:
+                _a.sent();
+                hasAvail = Object.entries(availability_3).filter(function (date) { return date[1].remaining > 0; });
+                if (hasAvail.length) {
+                    message = "Hey! Elizabeth Lake Head has availability: ".concat(hasAvail
+                        .map(function (date) { return date[0] + ": " + date[1].remaining + " remaining sites"; })
+                        .join(", "));
+                    twilioClient.messages
+                        .create({ body: message, from: "+18668414666", to: "+17163615473" })
+                        .then(function (msg) { return console.log(msg.sid); });
+                }
+                _a.label = 15;
+            case 15:
+                response.ELH = { dateTime: Date.now(), filteredDates: filteredDates };
+                return [3 /*break*/, 17];
+            case 16:
+                error_3 = _a.sent();
+                console.log("ELH error", error_3);
+                return [3 /*break*/, 17];
+            case 17:
+                _a.trys.push([17, 21, , 22]);
+                return [4 /*yield*/, axios_1.default.get("https://www.recreation.gov/api/permititinerary/4675321/division/4675321022/availability/month?month=8&year=2023")];
+            case 18:
+                GAB = _a.sent();
+                filteredDates = Object.entries(GAB.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter(function (entry) {
+                    return ["2023-08-09"].includes(entry[0]);
+                });
+                if (!filteredDates.length) return [3 /*break*/, 20];
+                availability_4 = {};
+                filteredDates.forEach(function (date) {
+                    availability_4[date[0]] = { total: date[1].total, remaining: date[1].remaining };
+                });
+                requests = (0, lite_1.collection)(db, "GAB");
+                return [4 /*yield*/, (0, lite_1.addDoc)(requests, { dateTime: Date.now(), availability: availability_4 })];
+            case 19:
+                _a.sent();
+                hasAvail = Object.entries(availability_4).filter(function (date) { return date[1].remaining > 0; });
+                if (hasAvail.length) {
+                    message = "Hey! Gable Creek has availability: ".concat(hasAvail
+                        .map(function (date) { return date[0] + ": " + date[1].remaining + " remaining sites"; })
+                        .join(", "));
+                    twilioClient.messages
+                        .create({ body: message, from: "+18668414666", to: "+17163615473" })
+                        .then(function (msg) { return console.log(msg.sid); });
+                }
+                _a.label = 20;
+            case 20:
+                response.GAB = { dateTime: Date.now(), filteredDates: filteredDates };
+                return [3 /*break*/, 22];
+            case 21:
+                error_4 = _a.sent();
+                console.log("GAB error", error_4);
+                return [3 /*break*/, 22];
+            case 22:
+                console.log("CRON RESP", response);
+                return [2 /*return*/, res.status(200).send(response)];
+            case 23:
+                error_5 = _a.sent();
+                console.error(error_5);
+                return [2 /*return*/, res.status(500).send(error_5)];
+            case 24: return [2 /*return*/];
         }
     });
 }); });
 app.get("/requests", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var requestsCol, docs, response;
+    var STOCol, STOdocs, ELFCol, ELFdocs, ELHCol, ELHdocs, GABCol, GABdocs, response_1, error_6;
     return __generator(this, function (_a) {
         switch (_a.label) {
             case 0:
-                requestsCol = (0, lite_1.query)((0, lite_1.collection)(db, "requests").withConverter(converter), (0, lite_1.orderBy)("dateTime", "desc"), (0, lite_1.limit)(100));
-                return [4 /*yield*/, (0, lite_1.getDocs)(requestsCol)];
+                _a.trys.push([0, 5, , 6]);
+                STOCol = (0, lite_1.query)((0, lite_1.collection)(db, "STO").withConverter(converter), (0, lite_1.orderBy)("dateTime", "desc"), (0, lite_1.limit)(100));
+                return [4 /*yield*/, (0, lite_1.getDocs)(STOCol)];
             case 1:
-                docs = _a.sent();
-                response = [];
-                docs.forEach(function (doc) { return response.push(doc.data()); });
-                return [2 /*return*/, res.status(200).send(response)];
+                STOdocs = _a.sent();
+                ELFCol = (0, lite_1.query)((0, lite_1.collection)(db, "ELF").withConverter(converter), (0, lite_1.orderBy)("dateTime", "desc"), (0, lite_1.limit)(100));
+                return [4 /*yield*/, (0, lite_1.getDocs)(ELFCol)];
+            case 2:
+                ELFdocs = _a.sent();
+                ELHCol = (0, lite_1.query)((0, lite_1.collection)(db, "ELH").withConverter(converter), (0, lite_1.orderBy)("dateTime", "desc"), (0, lite_1.limit)(100));
+                return [4 /*yield*/, (0, lite_1.getDocs)(ELHCol)];
+            case 3:
+                ELHdocs = _a.sent();
+                GABCol = (0, lite_1.query)((0, lite_1.collection)(db, "GAB").withConverter(converter), (0, lite_1.orderBy)("dateTime", "desc"), (0, lite_1.limit)(100));
+                return [4 /*yield*/, (0, lite_1.getDocs)(GABCol)];
+            case 4:
+                GABdocs = _a.sent();
+                response_1 = { STO: [], ELF: [], ELH: [], GAB: [] };
+                STOdocs.forEach(function (doc) { return response_1.STO.push(doc.data()); });
+                ELFdocs.forEach(function (doc) { return response_1.ELF.push(doc.data()); });
+                ELHdocs.forEach(function (doc) { return response_1.ELH.push(doc.data()); });
+                GABdocs.forEach(function (doc) { return response_1.GAB.push(doc.data()); });
+                console.log("/requests response", response_1);
+                return [2 /*return*/, res.status(200).send(response_1)];
+            case 5:
+                error_6 = _a.sent();
+                console.log("/REQUESTS error:", error_6);
+                return [3 /*break*/, 6];
+            case 6: return [2 /*return*/];
         }
     });
 }); });
--- api/src/index.ts
+++ api/src/index.ts
@@ -15,17 +15,7 @@ const twilioClient = twilio(accountSid, authToken);
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  query,
-  QueryDocumentSnapshot,
-  orderBy,
-  limit,
-} from "firebase/firestore/lite";
-
+import { getFirestore, collection, getDocs, addDoc, query, QueryDocumentSnapshot, orderBy, limit } from "firebase/firestore/lite";
 
 // Initialize Firestore through Firebase
 const firebaseConfig = {
@@ -51,36 +41,166 @@ app.all("/", (req, res) => {
 });
 
 app.get("/cron", async (req, res) => {
+  console.log("CRON!");
+  const response: {
+    [key: string]: {
+      dateTime: number;
+      filteredDates: [
+        string,
+        {
+          total: number;
+          remaining: number;
+          show_walkup: boolean;
+          is_hidden: boolean;
+          season_type: string;
+        }
+      ][];
+    };
+  } = {};
   try {
-    const response = await axios.get<AvailabilityResponse>(
-      "https://www.recreation.gov/api/permititinerary/4675321/division/4675321007/availability/month?month=8&year=2023"
-    );
+    // STONEY INDIAN
+    try {
+      const STO = await axios.get<AvailabilityResponse>(
+        "https://www.recreation.gov/api/permititinerary/4675321/division/4675321007/availability/month?month=8&year=2023"
+      );
 
-    const filteredDates = Object.entries(response.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter((entry) =>
-      ["2023-08-11", "2023-08-12"].includes(entry[0])
-    );
-    if (filteredDates.length) {
-      const availability: { [date: string]: { total: number; remaining: number } } = {};
-      filteredDates.forEach((date) => {
-        availability[date[0]] = { total: date[1].total, remaining: date[1].remaining };
-      });
+      const filteredDates = Object.entries(STO.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter((entry) =>
+        ["2023-08-11", "2023-08-12"].includes(entry[0])
+      );
+      if (filteredDates.length) {
+        const availability: { [date: string]: { total: number; remaining: number } } = {};
+        filteredDates.forEach((date) => {
+          availability[date[0]] = { total: date[1].total, remaining: date[1].remaining };
+        });
 
-      const requests = collection(db, "requests");
-      await addDoc(requests, { dateTime: Date.now(), availability });
+        const requests = collection(db, "STO");
+        await addDoc(requests, { dateTime: Date.now(), availability });
 
-      const hasAvail = Object.entries(availability).filter((date) => date[1].remaining > 0);
+        const hasAvail = Object.entries(availability).filter((date) => date[1].remaining > 0);
 
-      if (hasAvail.length) {
-        const message = `Hey! Stony Indian Lake has availability: ${hasAvail
-          .map((date) => date[0] + ": " + date[1].remaining + " remaining sites")
-          .join(", ")}`;
+        if (hasAvail.length) {
+          const message = `Hey! Stony Indian Lake has availability: ${hasAvail
+            .map((date) => date[0] + ": " + date[1].remaining + " remaining sites")
+            .join(", ")}`;
 
-        twilioClient.messages
-          .create({ body: message, from: "+18668414666", to: "+17163615473" })
-          .then((msg) => console.log(msg.sid));
+          twilioClient.messages
+            .create({ body: message, from: "+18668414666", to: "+17163615473" })
+            .then((msg) => console.log(msg.sid));
+        }
       }
+      response.STO = { dateTime: Date.now(), filteredDates };
+    } catch (error) {
+      console.log("STO error", error);
     }
-    return res.status(200).send({ dateTime: Date.now(), filteredDates });
+
+    // ELF REQUEST
+    try {
+      const ELF = await axios.get<AvailabilityResponse>(
+        "https://www.recreation.gov/api/permititinerary/4675321/division/4675321020/availability/month?month=8&year=2023"
+      );
+
+      const filteredDates = Object.entries(ELF.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter((entry) =>
+        ["2023-08-09", "2023-08-10"].includes(entry[0])
+      );
+      if (filteredDates.length) {
+        const availability: { [date: string]: { total: number; remaining: number } } = {};
+        filteredDates.forEach((date) => {
+          availability[date[0]] = { total: date[1].total, remaining: date[1].remaining };
+        });
+
+        const requests = collection(db, "ELF");
+        await addDoc(requests, { dateTime: Date.now(), availability });
+
+        const hasAvail = Object.entries(availability).filter((date) => date[1].remaining > 0);
+
+        if (hasAvail.length) {
+          const message = `Hey! Elizabeth Lake Foot has availability: ${hasAvail
+            .map((date) => date[0] + ": " + date[1].remaining + " remaining sites")
+            .join(", ")}`;
+
+          twilioClient.messages
+            .create({ body: message, from: "+18668414666", to: "+17163615473" })
+            .then((msg) => console.log(msg.sid));
+        }
+      }
+      response.ELF = { dateTime: Date.now(), filteredDates };
+    } catch (error) {
+      console.log("ELF error", error);
+    }
+
+    // ELH REQUEST
+    try {
+      const ELH = await axios.get<AvailabilityResponse>(
+        "https://www.recreation.gov/api/permititinerary/4675321/division/4675321021/availability/month?month=8&year=2023"
+      );
+
+      const filteredDates = Object.entries(ELH.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter((entry) =>
+        ["2023-08-09", "2023-08-10"].includes(entry[0])
+      );
+      if (filteredDates.length) {
+        const availability: { [date: string]: { total: number; remaining: number } } = {};
+        filteredDates.forEach((date) => {
+          availability[date[0]] = { total: date[1].total, remaining: date[1].remaining };
+        });
+
+        const requests = collection(db, "ELH");
+        await addDoc(requests, { dateTime: Date.now(), availability });
+
+        const hasAvail = Object.entries(availability).filter((date) => date[1].remaining > 0);
+
+        if (hasAvail.length) {
+          const message = `Hey! Elizabeth Lake Head has availability: ${hasAvail
+            .map((date) => date[0] + ": " + date[1].remaining + " remaining sites")
+            .join(", ")}`;
+
+          twilioClient.messages
+            .create({ body: message, from: "+18668414666", to: "+17163615473" })
+            .then((msg) => console.log(msg.sid));
+        }
+      }
+      response.ELH = { dateTime: Date.now(), filteredDates };
+    } catch (error) {
+      console.log("ELH error", error);
+    }
+
+    // GAB REQUEST
+    try {
+      const GAB = await axios.get<AvailabilityResponse>(
+        "https://www.recreation.gov/api/permititinerary/4675321/division/4675321022/availability/month?month=8&year=2023"
+      );
+
+      const filteredDates = Object.entries(GAB.data.payload.quota_type_maps.ConstantQuotaUsageDaily).filter((entry) =>
+        ["2023-08-09"].includes(entry[0])
+      );
+      if (filteredDates.length) {
+        const availability: { [date: string]: { total: number; remaining: number } } = {};
+        filteredDates.forEach((date) => {
+          availability[date[0]] = { total: date[1].total, remaining: date[1].remaining };
+        });
+
+        const requests = collection(db, "GAB");
+        await addDoc(requests, { dateTime: Date.now(), availability });
+
+        const hasAvail = Object.entries(availability).filter((date) => date[1].remaining > 0);
+
+        if (hasAvail.length) {
+          const message = `Hey! Gable Creek has availability: ${hasAvail
+            .map((date) => date[0] + ": " + date[1].remaining + " remaining sites")
+            .join(", ")}`;
+
+          twilioClient.messages
+            .create({ body: message, from: "+18668414666", to: "+17163615473" })
+            .then((msg) => console.log(msg.sid));
+        }
+      }
+      response.GAB = { dateTime: Date.now(), filteredDates };
+    } catch (error) {
+      console.log("GAB error", error);
+    }
+
+    console.log("CRON RESP", response);
+
+    return res.status(200).send(response);
   } catch (error) {
     console.error(error);
     return res.status(500).send(error);
@@ -88,13 +208,30 @@ app.get("/cron", async (req, res) => {
 });
 
 app.get("/requests", async (req, res) => {
-  const requestsCol = query(collection(db, "requests").withConverter(converter), orderBy("dateTime", "desc"), limit(100));
-  const docs = await getDocs<ApiRequests>(requestsCol);
-  const response: ApiRequests[] = [];
+  try {
+    const STOCol = query(collection(db, "STO").withConverter(converter), orderBy("dateTime", "desc"), limit(100));
+    const STOdocs = await getDocs<ApiRequests>(STOCol);
 
-  docs.forEach((doc) => response.push(doc.data()));
+    const ELFCol = query(collection(db, "ELF").withConverter(converter), orderBy("dateTime", "desc"), limit(100));
+    const ELFdocs = await getDocs<ApiRequests>(ELFCol);
 
-  return res.status(200).send(response);
+    const ELHCol = query(collection(db, "ELH").withConverter(converter), orderBy("dateTime", "desc"), limit(100));
+    const ELHdocs = await getDocs<ApiRequests>(ELHCol);
+
+    const GABCol = query(collection(db, "GAB").withConverter(converter), orderBy("dateTime", "desc"), limit(100));
+    const GABdocs = await getDocs<ApiRequests>(GABCol);
+
+    const response: { [key: string]: ApiRequests[] } = { STO: [], ELF: [], ELH: [], GAB: [] };
+
+    STOdocs.forEach((doc) => response.STO.push(doc.data()));
+    ELFdocs.forEach((doc) => response.ELF.push(doc.data()));
+    ELHdocs.forEach((doc) => response.ELH.push(doc.data()));
+    GABdocs.forEach((doc) => response.GAB.push(doc.data()));
+    console.log("/requests response", response);
+    return res.status(200).send(response);
+  } catch (error) {
+    console.log("/REQUESTS error:", error);
+  }
 });
 
 console.log("RUN!");
